41ca9ccc27bfce61c8bf2308ceed736783d75b98
Kevin "Ilphrin" Pellet <ilphrin@autistici.org>
2018-03-29 20:08:56 -0400

    First usable version
    - Synchronize most background colors
    - Synchronize position, width, height
    - Synchronize text
    - Recursively recreate all the node tree of Figma Element

--- Synchronicity.js
+++ Synchronicity.js
@@ -5,6 +5,10 @@ export default class Synchronicity {
   constructor(fileName, configFile) {
     this._doc = null;
     this.domSynchro = new SynchroMap();
+    this.stylesheet = document.createElement('style');
+    this.width = 0;
+    this.height = 0;
+    document.body.appendChild(this.stylesheet);
 
     let req = new Request('https://api.figma.com/v1/files/' + fileName);
     fetch(req, {
@@ -60,17 +64,62 @@ export default class Synchronicity {
   update(e) {
     for (const [key, value] of this.domSynchro.entries()) {
       let figmaElement = this.searchFigmaElement(value);
+      this.width = figmaElement.absoluteBoundingBox.width;
+      this.height = figmaElement.absoluteBoundingBox.height;
       if (figmaElement !== null) {
         this.syncBackgroundColor(figmaElement, key);
+      }
+      this.generateContent(figmaElement, key);
+    }
+  }
+
+  generateContent(parentElement, domParent) {
+    for (let elem of parentElement.children) {
+      let child = document.createElement('div');
+      child.id = elem.name;
+      child.style.position = 'absolute';
+      child.style.left = Math.abs(elem.absoluteBoundingBox.x - parentElement.absoluteBoundingBox.x) + 'px';
+      child.style.top = Math.abs(elem.absoluteBoundingBox.y - parentElement.absoluteBoundingBox.y) + 'px';
+      child.style.width = elem.absoluteBoundingBox.width + 'px';
+      if (elem.type === "TEXT") {
+        Object.assign(child.style, elem.style);
+      }
+      child.style.height = elem.absoluteBoundingBox.height / window.innerHeight * 100 + 'vh';
+      if (elem.type === "GROUP" || elem.type === "RECTANGLE") {
+        if (elem.type === "RECTANGLE") {
+          // child.style.marginBottom = '-' + child.style.height;
+        }
+      }
+      if (elem.type === "TEXT") {
+        this.syncText(elem, child);
+      }
+      this.syncBackgroundColor(elem, child);
+      domParent.appendChild(child);
+      if (elem.children && elem.children.length !== 0) {
+        this.generateContent(elem, child);
       }
     }
   }
 
+  viewportPos(coord) {
+    return coord;
+  }
+
   syncBackgroundColor(figmaElement, domElem) {
-    if (figmaElement.backgroundColor) {
-      const color = Synchronicity.FigmaRGBAToHex(figmaElement.backgroundColor);
-      domElem.style.backgroundColor = color;
+    let color;
+    if (figmaElement.fills && figmaElement.fills.length !== 0 && figmaElement.fills[0].color) {
+      color = Synchronicity.FigmaRGBAToHex(figmaElement.fills[0].color);
     }
+    else if (figmaElement.backgroundColor) {
+      color = Synchronicity.FigmaRGBAToHex(figmaElement.backgroundColor);
+    }
+    else { return; }
+    this.stylesheet.innerHTML += `#${domElem.id} { background-color: ${color}; }`;
+  }
+
+  syncText(figmaElement, domElem) {
+    domElem.innerHTML = figmaElement.name;
+    domElem.style.color = Synchronicity.FigmaRGBAToHex(figmaElement.fills[0].color);
   }
 
   static FigmaRGBAToHex(rgba) {
